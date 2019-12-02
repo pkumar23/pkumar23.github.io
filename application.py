a9edefd29cfd25743bc96cf8af1dfdf88a4448d8
mychang07 <38193269+mychang07@users.noreply.github.com>
2019-12-02 14:33:03 -0800

    Update application.py

--- application.py
+++ application.py
@@ -3,13 +3,20 @@
 import os
 
 from cs50 import SQL
-from flask import Flask, flash, jsonify, redirect, render_template, request, session
+from authlib.integrations.flask_client import OAuth
+from flask import Flask, flash, jsonify, redirect, render_template, request, session, abort, url_for
 from flask_session import Session
+from functools import wraps
 from tempfile import mkdtemp
 from werkzeug.exceptions import default_exceptions, HTTPException, InternalServerError
 from werkzeug.security import check_password_hash, generate_password_hash
 
 from helpers import apology, login_required, lookup, usd
+
+# Check for environment variables
+for variable in ["CLIENT_ID", "CLIENT_SECRET", "SERVER_METADATA_URL"]:
+    if not os.environ.get(variable):
+        abort(500, f"Missing f{variable}")
 
 # Configure application
 app = Flask(__name__)
@@ -25,17 +32,52 @@ def after_request(response):
     response.headers["Pragma"] = "no-cache"
     return response
 
-# Custom filter
-app.jinja_env.filters["usd"] = usd
-
 # Configure session to use filesystem (instead of signed cookies)
 app.config["SESSION_FILE_DIR"] = mkdtemp()
 app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
+# Configure OAuth
+oauth = OAuth(app)
+oauth.register(
+    "cs50",
+    client_id=os.environ.get("CLIENT_ID"),
+    client_kwargs={"scope": "openid profile email"},
+    client_secret=os.environ.get("CLIENT_SECRET"),
+    server_metadata_url=os.environ.get("SERVER_METADATA_URL")
+)
+
+# Decorator to require login
+def login_required(f):
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        if session.get("userinfo") is None:
+            return redirect(url_for("login", next=request.url))
+        return f(*args, **kwargs)
+    return decorated_function
+
 # Configure CS50 Library to use SQLite database, will need tables for follower/followee + people at each location
 db = SQL("sqlite:///friends.db")
+
+@app.route("/")
+def index():
+    return render_template("index.html", userinfo=session.get("userinfo"))
+
+@app.route("/callback")
+def callback():
+    token = oauth.cs50.authorize_access_token()
+    session["userinfo"] = oauth.cs50.parse_id_token(token)
+    return redirect(url_for("index"))
+
+@app.route("/login")
+def login():
+    return oauth.cs50.authorize_redirect(redirect_uri=url_for("callback", _external=True))
+
+@app.route("/logout")
+def logout():
+    session.clear()
+    return redirect(url_for("index"))
 
 @app.route("/")
 @login_required
@@ -111,55 +153,6 @@ def friends():
     else:
         # query to get your friends and their current locations
         return render_template("friends.html", rows=rows)
-
-
-
-@app.route("/login", methods=["GET", "POST"]) # ?do we still need this if using Harvard authentication?
-def login():
-    """Log user in"""
-
-    # Forget any user_id
-    session.clear()
-
-    # User reached route via POST (as by submitting a form via POST)
-    if request.method == "POST":
-
-        # Ensure username was submitted
-        if not request.form.get("username"):
-            return apology("must provide username", 403)
-
-        # Ensure password was submitted
-        elif not request.form.get("password"):
-            return apology("must provide password", 403)
-
-        # Query database for username
-        rows = db.execute("SELECT * FROM users WHERE username = :username",
-                          username=request.form.get("username"))
-
-        # Ensure username exists and password is correct
-        if len(rows) != 1 or not check_password_hash(rows[0]["hash"], request.form.get("password")):
-            return apology("invalid username and/or password", 403)
-
-        # Remember which user has logged in
-        session["user_id"] = rows[0]["id"]
-
-        # Redirect user to home page
-        return redirect("/")
-
-    # User reached route via GET (as by clicking a link or via redirect)
-    else:
-        return render_template("login.html")
-
-
-@app.route("/logout")
-def logout():
-    """Log user out"""
-
-    # Forget any user_id
-    session.clear()
-
-    # Redirect user to login form
-    return redirect("/")
 
 
 
