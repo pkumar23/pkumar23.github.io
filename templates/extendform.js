238a3664e1b8090b88b9118eb635a2a4cb20b589
pkumar23 <54913928+pkumar23@users.noreply.github.com>
2019-12-03 21:02:56 -0500

    Create extendform.js

---
+++ templates/extendform.js
@@ -0,0 +1,26 @@
+const source = document.querySelector("#Location");
+const target1 = document.querySelector("#Dorms);
+const target2 = document.querySelector("#Houses);
+const target3 = document.querySelector("#Libraries);
+const target4 = document.querySelector("#Cafes);
+
+const displayWhenSelected = (source, value, target) => {
+    const selectedIndex = source.selectedIndex;
+    const isSelected = source[selectedIndex].value === value;
+    target.classList[isSelected
+        ? "add"
+        : "remove"
+    ]("show");
+};
+source.addEventListener("change", (evt) =>
+    displayWhenSelected(source, "First-Year Dorm", target1)
+);
+source.addEventListener("change", (evt) =>
+    displayWhenSelected(source, "Upperclass House", target2)
+);
+source.addEventListener("change", (evt) =>
+    displayWhenSelected(source, "Library", target3)
+);
+source.addEventListener("change", (evt) =>
+    displayWhenSelected(source, "Cafe", target4)
+);
